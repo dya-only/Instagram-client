b53eb6e2a6ee174fec3e031638a18c1320af0527
dya-only <dyacode@proton.me>
2023-10-08 15:20:00 +0900

    feat: search panel basics

--- src/components/Navigator/component.tsx
+++ src/components/Navigator/component.tsx
@@ -1,5 +1,5 @@
-import {ChangeEvent, Fragment, useEffect, useRef, useState} from "react"
-import {Link} from "react-router-dom"
+import { ChangeEvent, Fragment, useEffect, useRef, useState } from "react"
+import { Link } from "react-router-dom"
 import axios from "axios"
 
 import Instagram from "../../assets/svgs/Instagram.tsx"
@@ -16,14 +16,15 @@ import InstagramMini from "../../assets/svgs/InstagramMini.tsx"
 import Media from "../../assets/svgs/media.tsx"
 import Previous from "../../assets/svgs/previous.tsx"
 import Smile from "../../assets/svgs/smile.tsx"
-import {Container} from "../modal/container/component.tsx"
-import {Window} from "../modal/window/component.tsx"
+import { Container } from "../modal/container/component.tsx"
+import { Window } from "../modal/window/component.tsx"
 import Check from '../../assets/imgs/check.png'
 import Close from "../../assets/svgs/Close.tsx"
 
 export default function Navigator() {
   const [w, setW] = useState(658)
-  const [user, setUser] = useState({id: '', email: '', username: '', avatar: ''})
+  const [user, setUser] = useState({ id: '', email: '', username: '', avatar: '' })
+  const [isSearch, setIsSearch] = useState(false)
   const [content, setContent] = useState('')
   const [createStep, setCreateStep] = useState(0)
   const [preview, setPreview] = useState('')
@@ -32,27 +33,27 @@ export default function Navigator() {
 
   const userVerify = async () => {
     // AccessToken verify
-    axios.post('/api/auth/by-token', {token: sessionStorage.getItem('TOKEN')}, {
-      headers: {'Content-Type': 'application/json'}
+    axios.post('/api/auth/by-token', { token: sessionStorage.getItem('TOKEN') }, {
+      headers: { 'Content-Type': 'application/json' }
     }).then(resp => {
-        const res = resp.data
-        if (!res.success) return
+      const res = resp.data
+      if (!res.success) return
 
-        // Get user profile img
-        axios.get(`/api/user/${res.claims.id}`, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${sessionStorage.getItem('TOKEN')}`
-          }
-        }).then(_resp => {
-          setUser({
-            id: _resp.data.body.id,
-            email: _resp.data.body.email,
-            username: _resp.data.body.username,
-            avatar: _resp.data.body.avatar
-          })
+      // Get user profile img
+      axios.get(`/api/user/${res.claims.id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${sessionStorage.getItem('TOKEN')}`
+        }
+      }).then(_resp => {
+        setUser({
+          id: _resp.data.body.id,
+          email: _resp.data.body.email,
+          username: _resp.data.body.username,
+          avatar: _resp.data.body.avatar
         })
-      }
+      })
+    }
     )
   }
 
@@ -74,7 +75,7 @@ export default function Navigator() {
     content === '' ? formData.append('content', ' ') : formData.append('content', content)
 
     axios.post('/api/post', formData, {
-      headers: {'Authorization': `Bearer ${sessionStorage.getItem('TOKEN')}`}
+      headers: { 'Authorization': `Bearer ${sessionStorage.getItem('TOKEN')}` }
     }).then((resp) => console.log(resp.data))
   }
 
@@ -85,7 +86,6 @@ export default function Navigator() {
 
   return (
     <Fragment>
-
       {/* Upload post window */}
       {createStep ?
         <Container>
@@ -96,7 +96,7 @@ export default function Navigator() {
               게시물 만들기</div> : null}
             {createStep == 2 ?
               <div className={'w-full h-[42px] flex justify-between items-center border-b-[1px] border-b-gray-200'}>
-                <div className={'ml-4 cursor-pointer'} onClick={() => setCreateStep(1)}><Previous/></div>
+                <div className={'ml-4 cursor-pointer'} onClick={() => setCreateStep(1)}><Previous /></div>
                 <p className={'font-bold'}>자르기</p>
                 <p className={'text-blue-500 font-bold text-[15px] mr-4 cursor-pointer'} onClick={() => {
                   setCreateStep(3);
@@ -108,7 +108,7 @@ export default function Navigator() {
                 <div className={'ml-4 cursor-pointer'} onClick={() => {
                   setCreateStep(1);
                   setW(658)
-                }}><Previous/></div>
+                }}><Previous /></div>
                 <p className={'font-bold'}>새 게시물 만들기</p>
                 <p className={'text-blue-500 font-bold text-[15px] mr-4 cursor-pointer'} onClick={() => {
                   setCreateStep(4)
@@ -123,37 +123,37 @@ export default function Navigator() {
               : null}
 
             {createStep == 1 ? <div className={'w-[658px] h-[658px] flex flex-col justify-center items-center'}>
-              <Media w={96} h={20}/>
+              <Media w={96} h={20} />
               <p className={'text-xl font-[500]'}>사진과 동영상을 여기에 끌어다 놓지마세요</p>
 
               <button
                 className={'w-[120px] h-[32px] rounded-xl text-[15px] bg-[#0095F6] hover:bg-[#1877F2] text-white font-bold mt-6'}
                 onClick={() => fileRef.current?.click()}>컴퓨터에서 선택
               </button>
-              <input type='file' ref={fileRef} className={'hidden'} onChange={uploadFile}/>
+              <input type='file' ref={fileRef} className={'hidden'} onChange={uploadFile} />
             </div> : null}
             {createStep == 2 ?
-              <img className={'w-[658px] h-[659px] object-cover'} src={preview}/>
+              <img className={'w-[658px] h-[659px] object-cover'} src={preview} />
               : null}
             {createStep == 3 ?
               <div className={'flex justify-start items-start'}>
-                <img className={'w-[658px] h-[658px] object-cover rounded-bl-xl'} src={preview}/>
+                <img className={'w-[658px] h-[658px] object-cover rounded-bl-xl'} src={preview} />
                 <div className={'w-[339px] h-[275px] border-[1px] border-b-gray-300 p-5 bg-white'}>
                   <div className={'flex items-center mb-3'}>
-                    <img className={'w-[28px] h-[28px] rounded-full border-[1px] mr-3 object-cover'} src={`https://insta-clone-s3-bucket.s3.ap-northeast-2.amazonaws.com/${user.avatar}`}/>
+                    <img className={'w-[28px] h-[28px] rounded-full border-[1px] mr-3 object-cover'} src={`https://insta-clone-s3-bucket.s3.ap-northeast-2.amazonaws.com/${user.avatar}`} />
                     <p className={'font-bold text-sm'}>{user.username}</p>
                   </div>
 
                   <textarea className={'w-[290px] h-[175px] resize-none outline-none'} placeholder='문구 입력...'
-                            onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value)}/>
-                  <Smile/>
+                    onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value)} />
+                  <Smile />
                 </div>
               </div>
               : null}
             {createStep == 4 ?
-              <div className={'flex flex-col justify-start items-center'} onClick={() => {setCreateStep(0); window.location.href = `/profile`}}>
+              <div className={'flex flex-col justify-start items-center'} onClick={() => { setCreateStep(0); window.location.href = `/profile` }}>
                 <div className={'w-[658px] h-[658px] flex flex-col justify-center items-center'}>
-                  <img src={Check} alt="" className={'w-40'}/>
+                  <img src={Check} alt="" className={'w-40'} />
                   <div className={'text-2xl mt-4'}>게시물이 공유되었습니다.</div>
                 </div>
               </div>
@@ -164,95 +164,102 @@ export default function Navigator() {
 
       {/* Navigatior */}
       <nav
-        className={'fixed bg-white z-30 flex flex-col justify-between xs:items-start lg:items-start md:items-center sm:items-center xs:w-[244px] lg:w-[244px] md:w-[72px] p-4 xs:h-screen lg:h-screen md:h-screen sm:w-screen sm:h-[72px] xs:border-r-[1px] lg:border-r-[1px] md:border-r-[1px] sm:border-b-[1px] border-gray-300'}>
-        <div className="flex flex-col xs:items-start lg:items-start md:items-center sm:items-center">
-          <Link className={'xs:block lg:block md:hidden sm:hidden'} to={'/'} onClick={() => sessionStorage.setItem('status', 'Home')}><Instagram w={103} h={29}/></Link>
-          <Link className={'xs:hidden lg:hidden md:block sm:block'} to={'/'} onClick={() => sessionStorage.setItem('status', 'Home')}><InstagramMini/></Link>
+        className={`fixed bg-white z-30 flex flex-col justify-between xs:items-start lg:items-start md:items-center sm:items-center trnasition-all duration-300 xs:${isSearch ? 'w-[72px]' : 'w-[244px]'} lg:${isSearch ? 'w-[72px]' : 'w-[244px]'} md:w-[72px] p-4 xs:h-screen lg:h-screen md:h-screen sm:w-screen sm:h-[72px] xs:border-r-[1px] lg:border-r-[1px] md:border-r-[1px] sm:border-b-[1px] border-gray-300`}>
+        <div className="z-30 flex flex-col xs:items-start lg:items-start md:items-center sm:items-center">
+          { isSearch ?
+            <Link className={'block mt-[5px]'} to={'/'} onClick={() => sessionStorage.setItem('status', 'Home')}><InstagramMini /></Link>
+          : <Link className={'xs:block lg:block md:hidden sm:hidden'} to={'/'} onClick={() => sessionStorage.setItem('status', 'Home')}><Instagram w={103} h={29} /></Link> }
+          
+          <Link className={'xs:hidden lg:hidden md:block sm:block'} to={'/'} onClick={() => sessionStorage.setItem('status', 'Home')}><InstagramMini /></Link>
 
           {/*XS, LG*/}
-          <div className="xs:flex lg:flex md:hidden sm:hidden flex-col items-start">
-            <Link className={'flex justify-start items-end rounded-lg p-3 mb-4 w-[210px] hover:bg-gray-100'} to={'/'} onClick={() => sessionStorage.setItem('status', 'Home')}>
-              <span className={'mr-[15px] font-[noto]'}>{ sessionStorage.getItem('status') === 'Home' ? <Home/> : <EmptyHome/> }</span>
-              <p className={'font-[500] text-[16px]'}>홈</p>
+          <div className="z-30 xs:flex lg:flex md:hidden sm:hidden flex-col items-start">
+            <Link className={`flex justify-start items-end rounded-lg p-3 mb-4 ${isSearch ? 'w-[50px]' : 'w-[210px]'} hover:bg-gray-100`} to={'/'} onClick={() => sessionStorage.setItem('status', 'Home')}>
+              <span className={'mr-[15px] font-[noto]'}>{sessionStorage.getItem('status') === 'Home' ? <Home /> : <EmptyHome />}</span>
+              { isSearch ? null : <p className={'font-[500] text-[16px]'}>홈</p> }
             </Link>
 
-            <Link className={'flex justify-start items-end rounded-lg p-3 mb-4 w-[210px] hover:bg-gray-100'} to={''}>
-              <span className={'mr-[15px] font-[noto]'}><Search/></span>
-              <p className={'font-[500] text-[16px]'}>검색</p>
+            <div className={`cursor-pointer flex justify-start items-end rounded-lg p-3 mb-4 ${isSearch ? 'w-[50px]' : 'w-[210px]'} hover:bg-gray-100`} onClick={() => setIsSearch(isSearch ? false : true)}>
+              <span className={'mr-[15px] font-[noto]'}><Search /></span>
+              { isSearch ? null : <p className={'font-[500] text-[16px]'}>검색</p> }
+            </div>
+
+            <Link className={`flex justify-start items-end rounded-lg p-3 mb-4 ${isSearch ? 'w-[50px]' : 'w-[210px]'} hover:bg-gray-100`} to={''}>
+              <span className={'mr-[15px] font-[noto]'}><Explore /></span>
+              { isSearch ? null : <p className={'font-[500] text-[16px]'}>탐색 탭</p> }
             </Link>
 
-            <Link className={'flex justify-start items-end rounded-lg p-3 mb-4 w-[210px] hover:bg-gray-100'} to={''}>
-              <span className={'mr-[15px] font-[noto]'}><Explore/></span>
-              <p className={'font-[500] text-[16px]'}>탐색 탭</p>
+            <Link className={`flex justify-start items-end rounded-lg p-3 mb-4 ${isSearch ? 'w-[50px]' : 'w-[210px]'} hover:bg-gray-100`} to={''}>
+              <span className={'mr-[15px] font-[noto]'}><Reels /></span>
+              { isSearch ? null : <p className={'font-[500] text-[16px]'}>릴스</p> }
             </Link>
 
-            <Link className={'flex justify-start items-end rounded-lg p-3 mb-4 w-[210px] hover:bg-gray-100'} to={''}>
-              <span className={'mr-[15px] font-[noto]'}><Reels/></span>
-              <p className={'font-[500] text-[16px]'}>릴스</p>
+            <Link className={`flex justify-start items-end rounded-lg p-3 mb-4 ${isSearch ? 'w-[50px]' : 'w-[210px]'} hover:bg-gray-100`} to={''}>
+              <span className={'mr-[15px] font-[noto]'}><Message /></span>
+              { isSearch ? null : <p className={'font-[500] text-[16px]'}>메시지</p> }
             </Link>
 
-            <Link className={'flex justify-start items-end rounded-lg p-3 mb-4 w-[210px] hover:bg-gray-100'} to={''}>
-              <span className={'mr-[15px] font-[noto]'}><Message/></span>
-              <p className={'font-[500] text-[16px]'}>메시지</p>
+            <Link className={`flex justify-start items-end rounded-lg p-3 mb-4 ${isSearch ? 'w-[50px]' : 'w-[210px]'} hover:bg-gray-100`} to={''}>
+              <span className={'mr-[15px] font-[noto]'}><Heart w={24} h={24} /></span>
+              { isSearch ? null : <p className={'font-[500] text-[16px]'}>알림</p> }
             </Link>
 
-            <Link className={'flex justify-start items-end rounded-lg p-3 mb-4 w-[210px] hover:bg-gray-100'} to={''}>
-              <span className={'mr-[15px] font-[noto]'}><Heart w={24} h={24}/></span>
-              <p className={'font-[500] text-[16px]'}>알림</p>
-            </Link>
-
-            <button className={'flex justify-start items-end rounded-lg p-3 mb-4 w-[210px] hover:bg-gray-100'} onClick={() => setCreateStep(1)}>
-              <span className={'mr-[15px] font-[noto]'}><Create/></span>
-              <p className={'font-[500] text-[16px]'}>만들기</p>
+            <button className={`flex justify-start items-end rounded-lg p-3 mb-4 ${isSearch ? 'w-[50px]' : 'w-[210px]'} hover:bg-gray-100`} onClick={() => setCreateStep(1)}>
+              <span className={'mr-[15px] font-[noto]'}><Create /></span>
+              { isSearch ? null : <p className={'font-[500] text-[16px]'}>만들기</p> }
             </button>
 
-            <a className={'flex justify-start items-end rounded-lg p-3 mb-4 w-[210px] hover:bg-gray-100'} href={`/profile/${user.username}`} onClick={() => sessionStorage.setItem('status', 'Profile')}>
-              <img className={'w-[24px] h-[24px] mr-[15px] font-[noto] rounded-full object-cover'}
-                   src={`https://insta-clone-s3-bucket.s3.ap-northeast-2.amazonaws.com/${user.avatar}`} alt=""/>
-              <p className={'font-[500] text-[16px]'}>프로필</p>
+            <a className={`flex justify-start items-end rounded-lg p-3 mb-4 ${isSearch ? 'w-[50px]' : 'w-[210px]'} hover:bg-gray-100`} href={`/profile/${user.username}`} onClick={() => sessionStorage.setItem('status', 'Profile')}>
+              <img className={`w-[24px] h-[24px] ${isSearch ? '': 'mr-[15px]'} font-[noto] rounded-full object-cover`}
+                src={`https://insta-clone-s3-bucket.s3.ap-northeast-2.amazonaws.com/${user.avatar}`} alt="" />
+              { isSearch ? null : <p className={'font-[500] text-[16px]'}>프로필</p> }
             </a>
           </div>
 
           {/*MD, SM*/}
           <div className="xs:hidden lg:hidden md:block sm:block">
             <Link className={'flex justify-center items-end mb-8'} to={'/'}>
-              <span className={'font-[noto]'}>{ sessionStorage.getItem('status') === 'Home' ? <Home/> : <EmptyHome/> }</span>
+              <span className={'font-[noto]'}>{sessionStorage.getItem('status') === 'Home' ? <Home /> : <EmptyHome />}</span>
             </Link>
 
             <a className={'flex justify-center items-end mb-8'} href={''}>
-              <span className={'font-[noto]'}><Search/></span>
+              <span className={'font-[noto]'}><Search /></span>
             </a>
 
             <a className={'flex justify-center items-end mb-8'} href={''}>
-              <span className={'font-[noto]'}><Explore/></span>
+              <span className={'font-[noto]'}><Explore /></span>
             </a>
 
             <a className={'flex justify-center items-end mb-8'} href={''}>
-              <span className={'font-[noto]'}><Reels/></span>
+              <span className={'font-[noto]'}><Reels /></span>
             </a>
 
             <a className={'flex justify-center items-end mb-8'} href={''}>
-              <span className={'font-[noto]'}><Message/></span>
+              <span className={'font-[noto]'}><Message /></span>
             </a>
 
             <a className={'flex justify-center items-end mb-8'} href={''}>
-              <span className={'font-[noto]'}><Heart w={24} h={24}/></span>
+              <span className={'font-[noto]'}><Heart w={24} h={24} /></span>
             </a>
 
             <button className={'flex justify-center items-end mb-8'} onClick={() => setCreateStep(1)}>
-              <span className={'font-[noto]'}><Create/></span>
+              <span className={'font-[noto]'}><Create /></span>
             </button>
 
             <Link className={'flex justify-center items-end mb-8'} to={'/profile'} onClick={() => sessionStorage.setItem('status', 'Profile')}>
-              <img className={'w-[24px] h-[24px] font-[noto] rounded-full object-cover'} src={`https://insta-clone-s3-bucket.s3.ap-northeast-2.amazonaws.com/${user.avatar}`} alt=''/>
+              <img className={'w-[24px] h-[24px] font-[noto] rounded-full object-cover'} src={`https://insta-clone-s3-bucket.s3.ap-northeast-2.amazonaws.com/${user.avatar}`} alt='' />
             </Link>
           </div>
         </div>
 
-        <a className={'flex justify-center items-end xs:ml-2 lg:ml-2 md:ml-0 sm:ml-0ㅇ mb-4'} href={''}>
-          <span className={'font-[noto]'}><About/></span>
-          <p className={'font-[500] text-[16px] ml-[15px] xs:block lg:block md:hidden sm:hidden'}>더보기</p>
+        <a className={'flex justify-center items-end xs:ml-2 lg:ml-2 md:ml-0 sm:ml-0 mb-4'} href={''}>
+          <span className={'font-[noto]'}><About /></span>
+          { isSearch ? null : <p className={'font-[500] w-[100px] text-[16px] ml-[15px] xs:block lg:block md:hidden sm:hidden'}>더보기</p> }
         </a>
+
+        <div className={`transition-all duration-300 fixed ${isSearch ? 'ml-14' : '-ml-[410px]'} -mt-4 w-[397px] h-screen rounded-r-3xl border-[1px] border-gray-200 bg-white drop-shadow-2xl`}>
+          검색
+        </div>
       </nav>
     </Fragment>
   )
